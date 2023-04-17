1600b8ba1f4042bd837285b552c3e5ae79caf4ac
redsun62 david <adam.david8296@gmail.com>
2023-04-17 15:04:23 -0700

    removed unnecessary variables and implemented slice

--- stringContainsSubstring.js
+++ stringContainsSubstring.js
@@ -1,30 +1,13 @@
 const stringContainsSubstring = (str, subStr) => {
-  let subStrIndexOccurance = -1;
   const strLen = str.length;
   const subStrLen = subStr.length;
-  let charsRemaining = strLen;
-  let readingCounter = 0;
   if (strLen < subStrLen) {
-    return subStrIndexOccurance;
-  };
-  for (let i = 0; i < strLen; i++) {
-    if (charsRemaining < subStrLen) {
-      return subStrIndexOccurance;
-    };
-    charsRemaining--;
-    if (str[i] === subStr[0]) {
-      for (let j = 0; j < subStrLen; j++) {
-        if (str[i+j] !== subStr[j]) {
-          readingCounter = 0;
-        };
-        if (str[i+j] === subStr[j]) {
-          readingCounter++;
-        };
-        if (readingCounter === subStrLen) {
-          return i;
-        };
-      };
-    };
-  };
-  return subStrIndexOccurance;
+    return -1;
+  }
+  for (let i = 0; i <= strLen - subStrLen; i++) {
+    if (str.slice(i, i + subStrLen) === subStr) {
+      return i;
+    }
+  }
+  return -1;
 };
